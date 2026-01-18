5936d7d5b0ad5eb802ed2c5a4b243892996c0933
Vaishnavi Iyer <iyervaishnavi60@gmail.com>
2026-01-18 14:22:26 +0530

    star pattren new

--- starpattern1.java
+++ starpattern1.java
@@ -215,42 +215,109 @@ class Star{
             System.out.print("\n");
         }
     }
+    void pat15(int n )
+    {
+        int space =0;
 
+        for( int i=0;i<n;i++)
+        {
+            //stars 
+            for( int j=1;j<=n-i;j++)
+            {
+                System.out.print("*");
+            }
+            // spaces 
+            for (int j=0;j<space;j++)
+            {
+                System.out.print(" ");
+            }
+            //stars 
+            for( int j=1;j<=n-i;j++)
+            {
+                System.out.print("*");
+            }
+            space+=2;
+            System.out.print("\n");
+
+            
+
+
+        }
+    
+    space=8;
+    
+    for ( int i=0;i<n;i++)
+    {
+
+        //stars
+        for ( int j=0;j<=i;j++)
+        {
+            System.out.print("*");
+        }
+        //space
+        for( int j=0;j<space;j++)
+        {
+            System.out.print(" ");
+        }
+         //stars
+        for ( int j=0;j<=i;j++)
+        {
+            System.out.print("*");
+        }
+        space-=2;
+        System.out.print("\n");
+        
+    }
+    
+
+    }
 }
 
 public class starpattern1 {
     public static void main(String[] args) {
         Star p1=new Star();
-       
+        System.out.println("  pat 1 \n");
         p1.pat1(5);
-        System.out.println(" \n");
+        System.out.println("  pat 2 \n");
         p1.pat2(5);
-        System.out.println("\n");
+        System.out.println(" pat 3 \n");
         p1.pat3(5);
-         System.out.println("\n");
+         System.out.println(" pat 4 \n");
         p1.pat4(5);
-        System.out.println("\n");
+        System.out.println(" pat 5 \n");
         p1.pat5(5);
-        System.out.println("\n");
+        System.out.println(" pat 6 \n");
         p1.pat6(5);
-        System.out.println("\n");
+        System.out.println(" pat 7 \n");
         p1.pat7(5);
-        System.out.println("\n");
+        System.out.println(" pat 8 \n");
         p1.pat8(8);
-        System.out.println("\n");
+        System.out.println(" pat 9 \n");
         p1.pat9(10);
-        System.out.println("\n");
+        System.out.println(" pat 10 \n");
         p1.pat10(8);
-        System.out.println("\n");
+        System.out.println(" pat 11 \n");
         p1.pat11(8);
-        System.out.print("\n");
+        System.out.print(" pat 12 \n");
         p1.pat12(5);
-        System.out.print("\n");
+        System.out.print(" pat 13 \n");
         p1.pat13(6);
-        System.out.print("\n");
+        System.out.print(" pat 14 \n");
         p1.pat14(5);
+        System.out.print(" pat 15 \n");
+        p1.pat15(5);
+
 
 
     }
 
-}
+
+}   
+
+
+
+
+
+
+
+
