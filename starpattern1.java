8695520f6f20d4ca188c5f761eb8bd4e99e0f3c7
Vaishnavi Iyer <iyervaishnavi60@gmail.com>
2026-01-18 15:47:19 +0530

    new pattren

--- starpattern1.java
+++ starpattern1.java
@@ -1,3 +1,6 @@
+
+import java.awt.SystemColor;
+
 class Star{
     
     void pat1(int n )
@@ -300,6 +303,39 @@ class Star{
         }
         
     }
+    void pat17( int n){
+        for ( int i=0;i<n;i++)
+        {
+            if(i==0 || i==n-1)
+            {
+            //stars 
+                int stars =n;
+                for ( int j =0;j<stars;j++)
+                {
+                    System.out.print("*");
+                } 
+             
+            }
+            else 
+            
+            {
+               System.out.print("*");
+               for( int j=0;j<n-2;j++)
+               {
+                System.out.print(" ");
+               }
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+
+    }
+    /*easy approach 
+    if (i==0|| j==0 || i==n-1 || j==n-1 )
+    System.out.print("*");
+    else System.out.print(" "); 
+    */
 }
 
 public class starpattern1 {
@@ -337,6 +373,8 @@ public class starpattern1 {
         p1.pat15(5);
         System.out.println(" pat 16 \n ");
         p1.pat16(5);
+        System.out.println( " pat 17 \n ");
+        p1.pat17( 4);
 
 
 
